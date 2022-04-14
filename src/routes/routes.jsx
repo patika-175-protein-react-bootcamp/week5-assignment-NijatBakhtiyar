2b8e9eb3b93a145684d25fa14121b436c53fffb2
NijatBakhtiyar <NijatBakhtiyar-iatc>
2022-04-14 19:52:48 +0400

    fixed route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "../pages/register";
 
 function Routers() {
     return (
         <Routes>
             <Route path="/auth" element={<Register />} />
-            <Route path="*" element={<Register />} />
+            <Route path="*" element={<Navigate to="/auth" />} />
         </Routes>
     );
 }
